13dc5696c6d701d4b082de2a836e0a886322753f
Krisna Pranav <krisna.pranav@gmail.com>
2022-05-08 16:09:34 +0530

    netkit: settings

--- netkit/settings.h
+++ netkit/settings.h
@@ -15,4 +15,17 @@
 #define CAPTURE_RECV    (0x02)
 #define CAPTURE_ALL    (CAPTURE_SEND|CAPTURE_RECV)
 
+namespace netkit {
+    namespace settings {
+        extern size_t        REFRESH_SECS;
+        extern int        CAPTURE_ASR;
+        extern bool        ORDER_TOP;
+        extern bool        FILTER_ZERO;
+        extern bool        TCP_UDP_TRAFFIC;
+        extern bool        NO_RESOLVE;
+        extern std::string    ASYNC_LOG_FILE;
+        extern size_t        LIMIT_HOSTS_ROWS;
+    }
+}
+
 #endif /* settings_h */
