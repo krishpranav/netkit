4d9b863637b42c3d8524d3e61971b8504bcc0b45
Krisna Pranav <krisna.pranav@gmail.com>
2022-05-08 16:08:26 +0530

    netkit: settings[capture(SEND + RECV + ALL)]

---
+++ netkit/settings.h
@@ -0,0 +1,18 @@
+//
+//  settings.h
+//  netkit
+//
+//  Created by KrisnaPranav on 08/05/22.
+//
+
+#ifndef settings_h
+#define settings_h
+
+#include <cstdlib>
+#include <string>
+
+#define CAPTURE_SEND    (0x01)
+#define CAPTURE_RECV    (0x02)
+#define CAPTURE_ALL    (CAPTURE_SEND|CAPTURE_RECV)
+
+#endif /* settings_h */
